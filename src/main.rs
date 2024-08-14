87ebaf963d7b7f98f2002f6b0a12cc513b3e6f83
Cyber-Freak999 <cyberfreak999@protonmail.com>
2024-08-14 01:28:25 +0100

    Initialised DB and Added create and get articles Endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,55 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use rusqlite::{Connection, Result};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Article {
+    id: i32,
+    title: String,
+    content: String,
+}
+
+fn init_db() -> Result<()> {
+    let conn = Connection::open("blog.db")?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS articles (
+            id      INTEGER PRIMARY KEY,
+            title   TEXT NOT NULL,
+            content TEXT NOT NULL
+        )",
+        [],
+    )?;
+    Ok(())
+}
+
+async fn create_post(article: web::Json<Article>) -> impl Responder {
+    let conn = Connection::open("blog.db").unwrap();
+    conn.execute(
+        "INSERT INTO articles (title, content) VALUES (?1, ?2)",
+        (&article.title, &article.content),
+    )
+    .unwrap();
+
+    HttpResponse::Ok().json(article.into_inner())
+}
+
+async fn get_article(web::Path(id): web::Path<i32>) -> impl Responder {
+    let conn = Connection::open("blog.db").unwrap();
+    let mut stmt = conn
+        .prepare("SELECT id, title, content FROM articles WHERE id = ?1")
+        .unwrap();
+    let post = stmt
+        .query_row([id], |row| {
+            Ok(Article {
+                id: row.get(0)?,
+                title: row.get(1)?,
+                content: row.get(2)?,
+            })
+        })
+        .unwrap();
+
+    HttpResponse::Ok().json(post)
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
